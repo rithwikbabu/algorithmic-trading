857b2a917c063b309ea3de514a7d187a1c936074
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-12-04 20:15:32 -0600

    added process v1

--- process_signal.py
+++ process_signal.py
@@ -1,10 +1,34 @@
 import sys
+from datetime import datetime
 
-def process_signal(title, body):
-    # Your processing logic goes here
-    # For now, it just prints the title and body
-    print(f"Issue Title: {title}")
-    print(f"Issue Body: {body}")
+def process_signal(body):
+    _, body_1 = body.split(" symbol ")
+    SYMBOL, body_2 = body_1.split(" last ")
+
+    dirty_price, body_3 = body_2.split(" target ")
+    PRICE = float(dirty_price)
+
+    dirty_target, body_4 = body_3.split(" stop ")
+    TARGET = float(dirty_target)
+
+    dirty_stop_loss, body_5 = body_4.split(" time ")
+    STOP_LOSS = float(dirty_stop_loss)
+
+    dirty_time, type = body_5.split(" bull_type ")
+    TIME = datetime.strptime(dirty_time, "%Y-%m-%d %H:%M:%S.%f")
+    
+    TYPE = int(type.strip())
+    
+    trade_data = {
+        "symbol": SYMBOL,
+        "price": PRICE,
+        "target": TARGET,
+        "stop_loss": STOP_LOSS,
+        "time": TIME,
+        "type": TYPE
+    }
+    
+    return trade_data
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -14,4 +38,12 @@ if __name__ == "__main__":
     issue_title = sys.argv[1]
     issue_body = sys.argv[2]
     
-    process_signal(issue_title, issue_body)
+    SUBJECT_MATCH = "Trading Signals"
+    
+    if SUBJECT_MATCH not in issue_title:
+        print("Invalid Issue Title")
+        sys.exit(1)
+    
+    results = process_signal(issue_body)
+    
+    print(results)
