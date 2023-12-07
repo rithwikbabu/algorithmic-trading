50c47fd2eb80ac1c3ceda92974296b6e3eaa7eca
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-12-06 22:12:18 -0600

    new position sizing strategy

--- execute_trade.py
+++ execute_trade.py
@@ -9,11 +9,12 @@ load_dotenv()
 ALPACA_KEY = os.getenv("ALPACA_KEY")
 ALPACA_SECRET = os.getenv("ALPACA_SECRET")
 
-
 api = tradeapi.REST(ALPACA_KEY, ALPACA_SECRET,
                     base_url='https://paper-api.alpaca.markets')
 
-# Function to process the trading signal
+# Define the proportions for each position
+POSITION_SIZES = [0.25, 0.15, 0.10, 0.10, 0.075,
+                  0.075, 0.075, 0.05, 0.05, 0.05, 0.0125, 0.0125]
 
 
 def execute_trade(signal):
@@ -21,10 +22,25 @@ def execute_trade(signal):
     price = round(signal['price'], 2)
     target = round(signal['target'], 2)
     stop_loss = round(signal['stop_loss'], 2)
-    trade_type = signal['type']  # Assuming 1 for buy and 2 for sell
 
-    # Convert datetime to string for order
-    time_str = signal['time'].strftime('%Y-%m-%d %H:%M:%S')
+    # Get account information
+    account = api.get_account()
+    account_balance = float(account.equity)
+
+    # Calculate the value for each position based on the current account balance
+    position_values = [size * account_balance for size in POSITION_SIZES]
+
+    # Get a list of current positions
+    positions = api.list_positions()
+
+    # Determine the next available position size
+    if len(positions) < len(POSITION_SIZES):
+        next_position_size = position_values[len(positions)]
+    else:
+        return "Maximum positions reached"
+
+    # Calculate the number of shares to buy for the next position
+    qty = int(next_position_size / price)
 
     side = "buy"
 
@@ -32,7 +48,7 @@ def execute_trade(signal):
     try:
         order = api.submit_order(
             symbol=symbol,
-            qty=1,  # Define quantity as per your requirement
+            qty=int(qty),  # Updated quantity based on position size
             side=side,
             type='limit',
             time_in_force='gtc',
@@ -45,12 +61,6 @@ def execute_trade(signal):
     except Exception as e:
         return str(e)
 
-
-test_signal = {
-    'symbol': 'IDXX',
-    'price': 466.66,
-    'target': 473.5780678996626,
-    'stop_loss': 460.2381932100337,
-    'time': datetime.datetime(2023, 12, 1, 14, 34, 55, 449134),
-    'type': 2
-}
+# Example usage
+# signal = {'symbol': 'AAPL', 'price': 150, 'target': 160, 'stop_loss': 145, 'time': datetime.datetime.now()}
+# execute_trade(signal)
